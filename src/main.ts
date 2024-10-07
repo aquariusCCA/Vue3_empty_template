b196a11cae4980c0f35f9ca8a15affaf8d2d343d
Kevin Xiao <aquarius.c.c.a@gmail.com>
2024-10-07 13:35:41 +0800

    feat: 安裝 Element Plus

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import App from './App.vue'
+import { createApp } from 'vue'
 
-createApp(App).mount('#app');
+// 引入 ElementPlus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// ElementPlus 國際化配置
+import zhTw from 'element-plus/dist/locale/zh-tw.mjs'
+
+const app = createApp(App)
+
+app.use(ElementPlus, {
+  locale: zhTw
+})
+
+
+app.mount('#app')
