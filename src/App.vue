304d2ab808836b6778e839f090eb41bfb14562aa
Kevin Xiao <aquarius.c.c.a@gmail.com>
2024-10-07 13:58:25 +0800

    feat: 安裝 Axios

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,15 @@
   <RouterView></RouterView>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import axios from 'axios';
+
+axios
+  .get('https://fakestoreapi.com/products/1')
+  .then((response) => {
+    console.log('Data:', response.data);
+  })
+  .catch((error) => {
+    console.error('Error fetching data:', error);
+  });
+</script>
