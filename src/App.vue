dfcaadd2f12ac49482979b63f2aeb58bbdae81da
Kevin Xiao <aquarius.c.c.a@gmail.com>
2024-10-07 10:16:48 +0800

    test husky

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,44 @@
 <script setup lang="ts">
 defineProps({
-/* ✓ GOOD */
-msg: String,
-greetingText: String,
-/* ✗ BAD */
-// 'greeting-text': String,
-})
- 
+  /* ✓ GOOD */
+  msg: String,
+  greetingText: String
+  /* ✗ BAD */
+  // 'greeting-text': String,
+});
+
 function add() {
-return 1+1
+  return 1 + 1;
 }
-let x=add();
-console.log(x)
+let x = add();
+console.log(x);
 </script>
 
 <template>
-    <!-- ✓ GOOD -->
-<ol v-for="i in 5" :key="i">
-<li>{{ i }}</li>
-</ol>
+  <!-- ✓ GOOD -->
+  <ol v-for="i in 5" :key="i">
+    <li>{{ i }}</li>
+  </ol>
 
-<!-- ✗ BAD -->
-<!-- <ol v-for="i in 5">
+  <!-- ✗ BAD -->
+  <!-- <ol v-for="i in 5">
 <li>item</li>
 </ol> -->
 </template>
 
 <style scoped>
 .logo {
-height: 6em;
-padding: 1.5em;
-will-change: filter;
-transition: filter 300ms;
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
 
 .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
 
 .logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
